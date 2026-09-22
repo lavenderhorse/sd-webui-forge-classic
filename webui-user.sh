cedcaa9960a34687c9a9ea11f62f3ac1ee8fa15d
twilight <hollisternolan2@gmail.com>
2026-09-21 19:00:03 -0600

    Keep local changes: custom COMMANDLINE_ARGS and executable bits on webui scripts

--- webui-user.sh
+++ webui-user.sh
@@ -6,7 +6,7 @@
 
 # export TORCH_COMMAND="pip install torch==2.12.0 torchvision==0.27.0"
 
-export COMMANDLINE_ARGS="--uv"
+export COMMANDLINE_ARGS="--uv --listen --styles=pd-styles/*.csv"
 
 # --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
 
